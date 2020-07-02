a3368d43409da543cdc389824c3e931b0d34a491
Song Lee Rang <elf0508@naver.com>
2020-07-02 19:17:00 +0900

    수정

--- keras/keras110_1_sigmoid.py
+++ keras/keras110_1_sigmoid.py
@@ -2,6 +2,8 @@
 # 함수 값 중심이 0.5로 학습속도가 느리고, exp함수 사용시 연산비용이 많이 들고, 
 # 입력값이 일정이상 올라가면 기울기가 거의 0에 수렴하여, gradient vanishing현상이 발생한다.
 
+# 함수 설명 사이트 : https://ratsgo.github.io/deep%20learning/2017/04/22/NNtricks/
+
 import numpy as np
 import matplotlib.pyplot as plt
 
