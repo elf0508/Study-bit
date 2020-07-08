1d950f4b876a0e195ddb32aef5eef3a94bdc4f2b
Song Lee Rang <elf0508@naver.com>
2020-07-08 18:14:24 +0900

    2020-07-08

--- tf/tf10_1_diabetes.py
+++ tf/tf10_1_diabetes.py
@@ -1,58 +1,39 @@
-# 회귀
-
 from sklearn.datasets import load_diabetes
 import tensorflow as tf
 
+diabetes = load_diabetes()
 
-dataset = load_diabetes()
+x_data = diabetes.data
+y_data = diabetes.target
 
-data = dataset.data
-target = dataset.target
+y_data = y_data.reshape(-1, 1)
 
+print(x_data.shape)           # (442, 10)
+print(y_data.shape)           # (442,)
 
-print(data.shape) # (442, 10)
-print(target.shape) # (442, )
-
-tf.set_random_seed(777)
-
-                               
 x = tf.placeholder(tf.float32, shape=[None, 10])
-y = tf.placeholder(tf.float32, shape=[None])
+y = tf.placeholder(tf.float32, shape=[None, 1])
+
+w = tf.Variable(tf.random_normal([10, 1]), name = 'weight')
+                                # x의 열의 값과 동일 해야 함(행렬 계산) 
+
+b = tf.Variable(tf.random_normal([1]), name = 'bias') 
+
+hypothesis = tf.matmul(x, w) + b                      # wx + b (행렬 곱)
+
+cost =  tf.reduce_mean(tf.square( hypothesis - y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate= 1e-6) # 0.00001
 
 
-w = tf.Variable(tf.zeros([10, 1]), name = 'weight')
-b = tf.Variable(tf.zeros([1]), name = 'bias')
+train = optimizer.minimize(cost)
 
-hypothesis = tf.sigmoid(tf.matmul(x, w) + b)  
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
+for step in range(2001):
+    cost_val, hy_val, _= sess.run([cost, hypothesis, train],
+                                feed_dict = {x: x_data, y: y_data})
 
-cost = tf.reduce_mean(tf.square(hypothesis-y))
-
-cost = -tf.reduce_mean(y * tf.log(hypothesis) + (1 - y) * 
-                        tf.log(1 - hypothesis))
-
-                                                  # 1e-5 : 0.00001
-optimizer = tf.train.GradientDescentOptimizer(learning_rate = 1e-5)
-train = optimizer.minimize(cost)  # 최소값으로 만들기 위해 cost값을 minimize 한다.
-
-# 준비 상태
-predicted = tf.cast(hypothesis > 0.5, dtype = tf.float32)
-accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, y),
-                            dtype = tf.float32))
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-
-    for step in range(5001):
-        cost_val, _ = sess.run([cost, train], feed_dict={x : data, y : target})
-        if step % 200 == 0:
-            print(step, cost_val)
-
-# 실제 실행 되는 곳
-    h, c, a = sess.run([hypothesis, predicted, accuracy],
-                            feed_dict={x : data, y : target})
-
-    print("\n Hypothesis : ", h, "\n Correct (y) : ", 
-            "\n Accuracy : ", a)
-
-
+    if step % 20 == 0 :
+        print(step, 'cost :',cost_val, '\n 예측값 :', hy_val)
