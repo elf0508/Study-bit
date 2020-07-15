e4225736936c68d1dcc10c26fbe5e9c410195281
Song Lee Rang <elf0508@naver.com>
2020-07-15 15:03:28 +0900

    2020-07-15

---
+++ django/polls/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from polls.models import Question, Choice
+# from django.models import Question, Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
+
+
