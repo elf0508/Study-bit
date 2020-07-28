535d7505d584428e57f8c6f092bacfff5f2d089b
Song Lee Rang <elf0508@naver.com>
2020-07-28 17:44:35 +0900

    2020-07-28

--- 프로젝트_연습/얼굴인식.py
+++ 프로젝트_연습/얼굴인식.py
@@ -4,7 +4,8 @@ import cv2
 
 # dlib에 있는 정면 얼굴 검출기로 입력 사진 img에서 얼굴을 검출하여 faces로 반환
 face_detector = dlib.get_frontal_face_detector()
-img = cv2.imread("harry.jpg")
+img = cv2.imread("D:/Study-bit/img/harry.jpg")
+# img = cv2.imread("harry.jpg")
 faces = face_detector(img)
 
 # 결과 출력 & 이미지에 해당 검출 부분들을 빨간색(bgr순) 박스로 표시
