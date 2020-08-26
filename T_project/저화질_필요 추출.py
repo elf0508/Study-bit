53d9680b02bfe2d5e3718baaa84b3b942eadb901
Song Lee Rang <elf0508@naver.com>
2020-08-26 16:28:22 +0900

    2020-08-26

--- T_project/저화질_필요 추출.py
+++ T_project/저화질_필요 추출.py
@@ -79,7 +79,7 @@ def search(dirname):
     # return np.save('D:/Study-bit/T_project/Low_Resolution/Image4.npy', arr=f)
     # return np.save('D:/Study-bit/T_project/Low_Resolution/Image5.npy', arr=f)
 
-    return np.save('D:/Study-bit/T_project/Low_Resolution/Image92.npy', arr=f)
+    return np.save('D:/Study-bit/T_project/Low_Resolution/Image99.npy', arr=f)
 
     
 
@@ -88,6 +88,6 @@ def search(dirname):
 # search('D:/Study-bit/T_project/Low_Resolution/19062521')
 # search('D:/Study-bit/T_project/Low_Resolution/19062531')
 
-search('D:/Study-bit/T_project/Low_Resolution/19072311')
+search('D:/Study-bit/T_project/Low_Resolution/19072432')
 
 
